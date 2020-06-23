97f3ed0f1aec2f80b3dfc66459d6e46e201a404c
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-23 12:55:36 -0500

    first commit

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +11,7 @@ module Ejemplo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,5 +19,3 @@ module Ejemplo
     # the framework and any gems in your application.
   end
 end
-
-# config.assets.initialize_on_precompile = false
