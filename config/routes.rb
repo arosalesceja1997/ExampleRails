7d1db1bdf1550718e8a9408b710af4327884d744
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-23 12:42:11 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  get '/products/edit', to: 'products#edit', as: 'edit_product'
+  get '/products/:id', to: 'products#show', as: 'product'
+  post '/products/:id/edit', to: 'products#create'
+  patch '/products/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#destroy'
 end
